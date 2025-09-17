589711b594793e96802e9c26819b7fe66d3ad325
taesak <dkstnghksky@gmail.com>
2025-09-17 17:23:22 +0900

    four

--- World.java
+++ World.java
@@ -1,5 +1,6 @@
 public class World {
     public static void run() {
-        System.out.println("202214075");  
+        System.out.println("202214075");
+        System.out.println("hello world");  
     }
 }
